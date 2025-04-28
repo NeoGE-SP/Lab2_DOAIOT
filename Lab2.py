1e2272edef399f9f02e45a7a18757076541a94e7
Neo Gao En <neoge07.24@ichat.sp.edu.sg>
2025-04-28 15:29:38 +0800

    BMI calculator

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,34 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+
+def calculate_BMI(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height**2)
+    #print("BMI = " + str(bmi))
+    return bmi
+
+
+def classify_BMI(bmi_value):
+    if bmi_value < 18.5:
+        print("You are UNDERWEIGHT")
+    elif bmi_value >= 18.5 and bmi_value <= 25.0:
+        print("Your weight is NORMAL")
+    else:
+        print("You are OVERWEIGHT")
+    return
+
+
+def main():
+    bmi_output = calculate_BMI(1.73, 60)
+    print("BMI Value = " + str(bmi_output))
+    classify_BMI(bmi_output)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
